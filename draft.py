167303c8458bafcd383ae7ec6004ed091f1c770b
t-rexdex <tyler.dex@gmail.com>
2022-11-22 10:50:14 -0600

    Trying to figure out where to begin or what api I want to utilize for this project

--- draft.py
+++ draft.py
@@ -1,1 +1,1 @@
-something something start here
+# Trying to figure out where to begin or what api I want to utilize for this project. May use a weather api, maybe music, maybe add ML capabilities if I get that far
