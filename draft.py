aefc2ec36a417b7b10f1c3eaf67404870ac099ee
t-rexdex <tyler.dex@gmail.com>
2022-11-25 15:21:18 -0600

    Pushing to cloud

--- draft.py
+++ draft.py
@@ -1,1 +1,11 @@
 # Trying to figure out where to begin or what api I want to utilize for this project. May use a weather api, maybe music, maybe add ML capabilities if I get that far
+# using this file to understand how to load and use apis for my project
+# import numpy as np
+# import pandas as pd
+import json
+import requests
+
+# going to use operweathermap.org api,
+# free account lets us call current weather and 3hr 5 day forecasts with 60 calls/minute
+api_key = 'bd4cd26c3163150080ad612f6ee5eb36'
+print(api_key)
