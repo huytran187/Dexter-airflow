0678890cd9080e1ac172c534e9ee166de5ff5c8a
t-rexdex <tyler.dex@gmail.com>
2022-11-25 18:20:37 -0600

    Beginning branch for learning api use

--- draft.py
+++ draft.py
@@ -7,5 +7,7 @@ import requests
 
 # going to use operweathermap.org api,
 # free account lets us call current weather and 3hr 5 day forecasts with 60 calls/minute
-api_key = 'bd4cd26c3163150080ad612f6ee5eb36'
-print(api_key)
+api_key = 'bd4cd26c3163150080ad612f6ee5eb36' # api key from openweathermap.org
+base_url = 'https://api.openweathermap.org/data/2.5/weather?'
+city = input('To check the weather, please type the city you would like to see: ')
+print('You have typed ' + city)
